6fb9c8fcc7c3757f9268ce2b844c033edebe4bb4
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2024-08-24 18:25:22 +0300

    Add `shlvl` to Tide

--- tide-config.nix
+++ tide-config.nix
@@ -110,7 +110,7 @@
 	tide_python_color = "00AFAF";
 	tide_python_icon = "󰌠";
 	tide_right_prompt_frame_enabled = "false";
-	tide_right_prompt_items = "status cmd_duration context jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig time";
+	tide_right_prompt_items = "status cmd_duration context jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig shlvl time";
 	tide_right_prompt_prefix = "";
 	tide_right_prompt_separator_diff_color = "";
 	tide_right_prompt_separator_same_color = "";
