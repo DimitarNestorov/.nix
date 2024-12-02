60decad8b5a71b5f2cf98aa8005aa21ee1f81d0f
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2024-12-02 19:09:14 +0200

    Update to Home Manager 24.11

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 		darwin.inputs.nixpkgs.follows = "nixpkgs";
 		nix-index-database.url = "github:nix-community/nix-index-database";
 		nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-		home-manager.url = "github:nix-community/home-manager/release-24.05";
+		home-manager.url = "github:nix-community/home-manager/release-24.11";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 	};
 
